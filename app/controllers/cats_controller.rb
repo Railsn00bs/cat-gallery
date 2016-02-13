56960fe459681f61a02d539796aa60ffa873a533
Nick Johnstone <ncwjohnstone@gmail.com>
2016-02-13 16:11:53 +1300

    Upload and display all the cats!

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -2,4 +2,10 @@ class CatsController < ApplicationController
   def index
     # by default, show the user the index view
   end
+
+  def create
+    Cat.create!(:image_url => params[:cat][:image_url])
+
+    redirect_to :action => :index
+  end
 end
